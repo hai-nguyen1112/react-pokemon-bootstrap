879f5717cbbb09a974bba1fd57c66c9ba178f09c
hai-nguyen1112 <hainguyen871112@gmail.com>
2020-02-07 14:40:58 -0500

    fixex syntax error inside axiosinstance

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -6,8 +6,9 @@ switch (window.location.hostname) {
   case 'localhost':
     instance = axios.create({baseURL: 'http://localhost:3000/'})
     break
-  case 'pokemon-searcher.netlify.com'
+  case 'pokemon-searcher.netlify.com':
     instance = axios.create({baseURL: 'https://pokemon-searcher.netlify.com/'})
+    break
   default:
     instance = axios.create({baseURL: 'http://localhost:3000/'})
 }
